90bfd2cf0a5c63ede7c9138402b0ee3776d9f537
Peter Zou <2169463408@qq.com>
2023-12-02 20:05:45 +0800

    day1/

--- day1/day1_7/day1_7.c
+++ day1/day1_7/day1_7.c
@@ -1,6 +1,8 @@
 #include<Stdio.h>
+
 int main()
 {
+#if 0 
 	float num1,num2,product;
 	
 	printf("请输入第一个浮点数：");
@@ -13,4 +15,28 @@ int main()
 	printf("%.2f 乘于 %.2f等于%.2f\n",num1,num2,product);
 	
 	return 0; 
+#else 
+
+
+    double firstNumber, secondNumber, product;
+    printf("输入两个浮点数: ");
+ 
+    // 用户输入两个浮点数
+    scanf("%lf %lf", &firstNumber, &secondNumber);  
+ 
+    // 两个浮点数相乘
+    product = firstNumber * secondNumber;  
+ 
+    // 输出结果， %.2lf 保留两个小数点
+    printf("结果 = %.2lf", product);
+    
+    return 0;
+#endif
 }
+
+
+
+
+
+
+
