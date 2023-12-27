69f04e5ae66e754d21d98be3abb63441e270585f
Peter Zou <2169463408@qq.com>
2023-12-27 20:33:36 +0800

    day12

---
+++ day12/day12_1/day12_1.c
@@ -0,0 +1,15 @@
+#include<Stdio.h>
+int main()
+{
+	int n1,n2,i,gcd;
+	printf("请输入两个数，以空格分隔："); 
+	scanf("%d %d",&n1, &n2);
+	
+	for(i = 1;i<=n1 && i<=n2;++i)
+		{
+			if(n1%i == 0 && n2%i==0)
+				gcd = i;
+		}
+	printf("%d 和 %d 的最大公约数是 %d",n1,n2,gcd);
+	return 0;
+}
