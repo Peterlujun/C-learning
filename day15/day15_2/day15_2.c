b2ed92e13712bbdffab69302433443a6433eddb2
Peter Zou <2169463408@qq.com>
2023-12-31 22:44:18 +0800

    day15

---
+++ day15/day15_2/day15_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n1,n2,i,gcd,lcm;
+    printf("请输入两个数，以空格隔开：");
+    scanf("%d %d",&n1,&n2);
+
+    for(i = 1;i<=n1 && i<=n2;++i)
+		{
+			if(n1%i == 0 && n2%i==0)
+				gcd = i;
+		}
+    lcm= (n1*n2)/gcd;
+    printf("%d\n",lcm);
+    return 0;
+
+
+}
